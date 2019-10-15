185ae4d7caf30a67a7dc7cebfdd1de959fc49806
saiaraine <55821575+saiaraine@users.noreply.github.com>
2019-10-14 17:25:12 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(1215, 1000,);
+
+}
+
+function draw() {
+
+  if (mouseIsPressed) {
+    fill(0,100,0);
+  } else {
+    fill(0,100,0);
+  }
+  fill(150,20,255)
+  ellipse(mouseX, mouseY, 80, 80);
+}
